1b5a99136903983986e567f2ea117d73b04cb5e0
misternogood <114906700+misternogood@users.noreply.github.com>
2022-11-10 12:08:04 +0900

    createアクション　ルーティング設定アクション定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,21 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_shopper = OrderShopper.new
   end
+
+  def create
+    @order_shopper = OrderShopper.new(order_params)
+    if @order_shopper.valid?
+      @order_shopper.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      @order_shopper = OrderShopper.new
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_shopper).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
