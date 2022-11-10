292ec4a14c6135e06ba50c291eb59cf3d23fc33b
misternogood <114906700+misternogood@users.noreply.github.com>
2022-11-10 13:20:02 +0900

    実装確認不具合修正

--- app/models/order_shopper.rb
+++ app/models/order_shopper.rb
@@ -18,7 +18,7 @@ class OrderShopper
   end
 
   def save
-    order = Order.create(user_id: user_id, item_id: user_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
     Shopper.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 
