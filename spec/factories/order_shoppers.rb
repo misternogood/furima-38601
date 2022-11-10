4261936f8ef5d59beda9bf97fa0ed2f3f3681633
misternogood <114906700+misternogood@users.noreply.github.com>
2022-11-10 16:52:41 +0900

    単体テストコード記述

---
+++ spec/factories/order_shoppers.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_shopper do
+    postal_code              { '123-4567' }
+    prefecture_id            { Faker::Number.between(from: 2, to: 48) }
+    city                     { '横浜市緑区' }
+    addresses                { '青山1-1-1' }
+    building                 { '柳ビル103' }
+    phone_number             { '08012345678' }
+  end
+end
